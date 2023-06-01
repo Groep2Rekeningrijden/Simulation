d249e187703a326e0517cb065f235208d9f82e00
David Hellinga <d.hellinga@student.fontys.nl>
2023-06-01 10:51:22 +0200

    Tracker uses carservice, sends coord and status

--- TrackerRunner/Program.cs
+++ TrackerRunner/Program.cs
@@ -11,8 +11,8 @@ namespace TrackerRunner
         private static int _batchSize;
         private static int _count;
         private static int _timeFactor;
-        private static readonly Random Rnd = new Random();
-        
+        private static readonly Random Rnd = new();
+
 
         static async Task Main(string[] args)
         {
@@ -52,11 +52,13 @@ namespace TrackerRunner
         private static string[] GetFiles()
         {
             //voor nick
-            var files = Directory.GetFiles(@"C:\Users\Nickv\Documents\School\Semester 6\sem 6 group\Simulation\TrackerRunner\Routes",
+            // var files = Directory.GetFiles(@"C:\Users\Nickv\Documents\School\Semester 6\sem 6 group\Simulation\TrackerRunner\Routes",
+            //     "*.json");
+
+            var files = Directory.GetFiles(@"/home/luna/Documents/rekeningrijden/Simulation/TrackerRunner/Routes",
                 "*.json");
 
-            //var files = Directory.GetFiles(@"/home/luna/Documents/rekeningrijden/Simulation/TrackerRunner/Routes",
-            //    "*.json");
+            // var files = Directory.GetFiles("Routes/");
 
             if (files.Length == 0)
             {
@@ -71,10 +73,7 @@ namespace TrackerRunner
             for (var i = 0; i < _count; i++)
             {
                 var file = files[Rnd.Next(files.Count)];
-                var task = Task.Run(async () =>
-                {
-                    await RunBatch(_batchSize, httpClient, _interval, file);
-                });
+                var task = Task.Run(async () => { await RunBatch(_batchSize, httpClient, _interval, file); });
 
                 tasks.Add(task);
             }
@@ -101,19 +100,39 @@ namespace TrackerRunner
                 timestamp = timestamp.AddSeconds(interval);
             }
 
+            // TODO: Move this to program argument or config file
+            const int batchesPerStatus = 10;
+            var ticksSinceStatus = 10;
 
             for (var j = 0; j < coordinates.Count; j += batchSize)
             {
+                if (ticksSinceStatus >= batchesPerStatus)
+                {
+                    await SendStatusAsync(httpClient, new StatusDto(id, 0));
+                    ticksSinceStatus = 0;
+                }
+
                 var batch = new RawInputDto(id, result.Skip(j).Take(batchSize).ToList());
                 await SendCoordinatesAsync(httpClient, batch);
                 await Task.Delay(_interval * _batchSize * 1000 / _timeFactor);
+                ticksSinceStatus++;
             }
+
+            await SendStatusAsync(httpClient, new StatusDto(id, 1));
         }
 
         private static async Task SendCoordinatesAsync(HttpClient httpClient, RawInputDto batch)
         {
             var content = new StringContent(JsonSerializer.Serialize(batch), Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync(_targetUrl, content);
+            var response = await httpClient.PostAsync($"{_targetUrl}raw", content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static async Task SendStatusAsync(HttpClient httpClient, StatusDto status)
+        {
+            // 0 if en route, 1 if ready
+            var content = new StringContent(JsonSerializer.Serialize(status), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync($"{_targetUrl}status", content);
             response.EnsureSuccessStatusCode();
         }
     }
